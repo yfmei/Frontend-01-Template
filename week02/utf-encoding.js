8dd48bb6c00f4b7a8938b6c2d389938b3d0b61c6
yfmei <15996213405@163.com>
2020-04-23 14:33:36 +0800

    完善utf-8编码

--- week02/utf-encoding.js
+++ week02/utf-encoding.js
@@ -2,66 +2,215 @@
  * @author yfmei
  * @date 2020/4/22
  */
+const BYTE_LEN = 8
 
 /**
- * 正则匹配所有 utf-8 encoding 字面量
+ * 分组
+ * @param arr
+ * @param childLen
+ * @returns {[]}
+ */
+function groupArr(arr, childLen = BYTE_LEN) {
+    let chunkedArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (i % childLen === 0) {
+            chunkedArr.push([])
+        }
+        chunkedArr[chunkedArr.length - 1].push(arr[i])
+    }
+    return chunkedArr
+}
+
+/**
+ * 正则匹配所有 utf-8 encode 字面量
  * 1. utf-8 针对 unicode 的可变长度字符编码
  * 2. 第一个字节兼容 ASCII 码
- * 3. 汉字要
+ * 3. 根据码点的2进制长度进行补位然后转为16进制
  * @param str
  * @returns {string}
  */
-function encoding(str) {
-    // 获取字符的 unicode 码点
-    let codePoint = str.codePointAt(str)
+function encode(str) {
+    let resultStr = []
+    for (let char of str) {
+        // 获取字符的 unicode 码点
+        let codePoint = char.codePointAt(char)
+        console.debug("码点为：" + codePoint)
 
-    // 补位
-    if (codePoint > 0 && codePoint < 127) {
-        console.log(`${str} 占一个字节, 补位为 0xxxxxxx`)
         // 转为二进制
         let binary = codePoint.toString(2)
-        let binaryReverse = "";
+
         let binaryLen = binary.length
-        console.log(binaryLen)
-        for(let i = binaryLen - 1; i < binaryLen; i--){
-            if (i < 0) {
-                break
-            }
-            binaryReverse += binary[i]
+        console.debug("码点二进制为：" + binary + ", " + binaryLen + "字节")
+        // 不足8位补0
+        let remains = binaryLen % 8
+
+        if (binaryLen > 8 && remains !== 0) {
+            console.debug("码点长度不是8的倍数，进行补0：" + remains)
+
+            binary = "0".repeat(BYTE_LEN - remains) + binary
+            console.debug("补0后码点二进制为：" + binary)
         }
-        let binaryRet = "0" + binaryReverse
-        console.log(binaryRet)
-        let finalByteLen = binaryRet.length
-        let bitLen = finalByteLen / 4
+
+        binaryLen = binary.length
+
+        // 补位
+        let binaryRet = ""
+        let firstByteCoverLen = "" // 第1个字节需补位长度
+        let secondByteCoverLen = "" // 第2个字节需补位长度
+        let thirdByteCoverLen = "" // 第3个字节需补位长度
+        let fourthByteCoverLen = "" // 第4个字节需补位长度
+        // 补位原则
+        // 0xxxxxxx
+        // 110xxxxx 10xxxxxx
+        // 1110xxxx 10xxxxxx 10xxxxxx
+        // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
+        if (codePoint < 128) {
+            console.debug(`${char} 占一个字节, 补位为 0xxxxxxx`)
+            firstByteCoverLen = BYTE_LEN - 1
+            binaryRet = "0" + binary.slice(0, firstByteCoverLen)
+            console.debug("补位后结果为：" + binaryRet)
+        } else if (codePoint > 127 && codePoint < 2048) {
+            firstByteCoverLen = BYTE_LEN - 3
+            binaryRet = "110" + binary.slice(0, firstByteCoverLen)
+            binaryRet += "10" + binary.slice(firstByteCoverLen, binaryLen - firstByteCoverLen - 1)
+        } else if (codePoint > 2047 && codePoint < 65536) {
+            firstByteCoverLen = BYTE_LEN - 4
+            secondByteCoverLen = firstByteCoverLen + 6
+            thirdByteCoverLen = secondByteCoverLen + 6
+
+            binaryRet = "1110" + binary.slice(0, firstByteCoverLen)
+            binaryRet += "10" + binary.slice(firstByteCoverLen, secondByteCoverLen)
+            binaryRet += "10" + binary.slice(secondByteCoverLen, thirdByteCoverLen)
+
+        } else if (codePoint > 65535 && codePoint <= 1114111) {
+            firstByteCoverLen = BYTE_LEN - 5
+            secondByteCoverLen = firstByteCoverLen + 6
+            thirdByteCoverLen = secondByteCoverLen + 6
+            fourthByteCoverLen = thirdByteCoverLen + 6
+            binaryRet = "11110" + binary.slice(0, firstByteCoverLen)
+            binaryRet += "10" + binary.slice(firstByteCoverLen, secondByteCoverLen)
+            binaryRet += "10" + binary.slice(secondByteCoverLen, thirdByteCoverLen)
+            binaryRet += "10" + binary.slice(thirdByteCoverLen, fourthByteCoverLen)
+        }
+        console.debug("补位结果为: " + binaryRet)
         let ret = ""
-        let point = 0
-        // 每4位转为16进制
-        for (let i = 0; i < bitLen; i++) {
-            let item = binaryRet[point] + binaryRet[point+1] + binaryRet[point+2] + binaryRet[point+3]
-            console.log(item)
-            ret += "" + parseInt(item, 2)
-            point += 4
+        // 分组, 每 8 位表示一个字节
+        let groupedArr = groupArr(binaryRet)
+        // 每个字节转为 2个 16 进制数
+        for (let item of groupedArr) {
+            let binary = parseInt(item.join(""), 2)
+            ret += binary.toString(16)
         }
-        console.log(ret)
-        return ret
-    }
-    return str
-}
 
-function decode(hexStr) {
-    var rawStr = hexStr.substr(0,2).toLowerCase() === "0x" ? hexStr.substr(2) : hexStr;
-    var len = rawStr.length;
-
-    var curCharCode;
-    var resultStr = [];
-    for(var i = 0; i < len;i = i + 2) {
-        curCharCode = parseInt(rawStr.substr(i, 2), 16);
-        resultStr.push(String.fromCharCode(curCharCode));
+        resultStr.push(ret)
     }
     return resultStr
 }
 
-let encodeStr = encoding("A")
-console.log("编码: " + encodeStr)
+/**
+ * 1. 判断补位格式（几个字节）
+ * 2. 反向移除补位
+ * 3. 获取码点
+ * 4. 转为 unicode 编码
+ * @param hexArr 16进制数组
+ * @returns {string}
+ */
+function decode(hexArr) {
+    let resultStr = ""
+    for (let i = 0; i < hexArr.length; i++) {
+        let hexStr = hexArr[i]
+        let coverCodePoint = "" // 补位结果
+        for (let i = 0; i < hexStr.length; i++) {
+            let hexArr = hexStr[i]
+            for (let hexChar of hexArr) {
+                // console.debug(hexChar)
+                // 单个字符的二进制
+                let binaryChar = parseInt(hexChar, 16).toString(2)
+                // console.debug(binaryChar)
+                // 不足4位补位
+                binaryChar = "0".repeat(4 - binaryChar.length) + binaryChar
+                // console.debug(binaryChar)
+                coverCodePoint += binaryChar
+            }
+        }
+        console.debug("解码获取补位结果：" + coverCodePoint)
+        // 4位一组划分16进制
+        let groupedHexArr = groupArr(coverCodePoint)
+        let codePoint = "" // 整个字符对应的码点
 
-console.log("解码: " + decode(encodeStr))
+        // 第一个字节
+        let firstChar = groupedHexArr[0].join("")
+        // 转为十进制
+        let maskFirstChar = parseInt(firstChar, 2)
+
+        console.debug("firstChar: " + firstChar)
+        // console.debug("maskFirstChar: " + maskFirstChar)
+
+        // 根据第一个字节的补位方式反向移除补位，获取码点
+
+        // 10000000 = 128
+        // 11000000 = 192
+        // 11100000 = 224
+        // 11110000 = 240
+
+        if ((maskFirstChar & 240) === 240) {
+            console.debug("firstChar: " + 240 .toString(2))
+            codePoint = firstChar.slice(5)
+            // 第二个字节
+            let secondChar = groupedHexArr[1].join("")
+            // 第三个字节
+            let thirdChar = groupedHexArr[2].join("")
+            // 第四个字节
+            let fourthChar = groupedHexArr[3].join("")
+            console.debug("第一个字符码点: " + codePoint)
+            codePoint += secondChar.slice(2)
+            codePoint += thirdChar.slice(2)
+            codePoint += fourthChar.slice(2)
+            console.debug("整个字符码点: " + codePoint)
+
+        } else if ((maskFirstChar & 224) === 224) {
+            console.debug("firstChar: " + 224 .toString(2))
+
+            codePoint = firstChar.slice(4)
+            let secondChar = groupedHexArr[1].join("")
+            let thirdChar = groupedHexArr[2].join("")
+            console.debug("第一个字符码点: " + codePoint)
+            codePoint += secondChar.slice(2)
+            codePoint += thirdChar.slice(2)
+            console.debug("整个字符码点: " + codePoint)
+        } else if ((maskFirstChar & 192) === 192) {
+            console.debug("firstChar: " + 192 .toString(2))
+
+            codePoint = firstChar.slice(3)
+            console.debug("第一个字符码点: " + codePoint)
+            let secondChar = groupedHexArr[1].join("")
+            let thirdChar = groupedHexArr[2].join("")
+
+            codePoint += secondChar.slice(2)
+            codePoint += thirdChar.slice(2)
+            console.debug("整个字符码点: " + codePoint)
+
+        } else if ((maskFirstChar & 128) === 0) {
+            console.debug("firstChar: " + 128 .toString(2))
+            codePoint = firstChar.slice(1)
+        }
+        console.debug("解码获取码点：" + codePoint)
+        console.debug("解码获取码点：" + parseInt(codePoint, 2))
+
+        // 码点转字符串
+        resultStr += String.fromCodePoint(parseInt(codePoint, 2))
+
+    }
+    return resultStr
+}
+
+
+let encodeArr = encode("A")
+console.info("UTF-8 编码: " + encodeArr)
+console.info("UTF-8 解码: " + decode(encodeArr))
+
+encodeArr = encode("解码获取补位结果")
+console.info("UTF-8 编码: " + encodeArr)
+console.info("UTF-8 解码: " + decode(encodeArr))
+
